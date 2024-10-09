5f34314631959f4fa94024905c485e5432fef9ae
nao.importa.web <nao.importa.web@gmail.com>
2024-10-09 19:11:41 -0300

    carga]

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,9 +1,9 @@
 
 rm -r /tmp/server
 mkdir -p /tmp/server/cml
-
 cp -r ../server/* /tmp/server/cml/
-
 rm -r /tmp/server/cml/data/*
+mkdir /tmp/server/cml/webpage/downloads
+tar -czvf /tmp/server/cml/webpage/downloads/client.tar.gz ../app
 
 
